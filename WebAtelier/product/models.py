14c09d3ae084da414c3afbb08d737b9d5790f705
Karel <edmolina4@gmail.com>
2019-05-05 19:11:34 -0600

    create products app

---
+++ WebAtelier/product/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Product(models.Model):
+    #TODO
+    # Name, Description,Price,Categorie,
+    # SKU,created, ...
+
+    #separate product options:
+    #Time, color, size,type
+
+    
+    pass
+
+class Categories(models.Model):
+    #TODO
+    #name, description
+    pass
+
+class ProductImage(models.Model):
+    #TODO
+    pass
+
+class Types(models.Model):
+    #TODO
+    pass
