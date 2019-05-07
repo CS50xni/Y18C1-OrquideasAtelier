1a9bf8f98d0c2219dc64fcf3a1b0338c186d4870
Karel <edmolina4@gmail.com>
2019-05-07 14:55:01 -0600

    set the basic product model

--- WebAtelier/product/models.py
+++ WebAtelier/product/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
-
 class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True, null=True )
+    price = models.DecimalField(decimal_places=2, max_digits=6, default=10.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.title
     #TODO
     # Name, Description,Price,Categorie,
     # SKU,created, ...
@@ -9,10 +16,10 @@ class Product(models.Model):
     #separate product options:
     #Time, color, size,type
 
-    
+
     pass
 
-class Categories(models.Model):
+class Category(models.Model):
     #TODO
     #name, description
     pass
