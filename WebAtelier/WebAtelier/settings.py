6e527ca6d451d32f7979416e3314478493c4c075
marioobr <obrlop@gmail.com>
2019-05-08 15:33:18 -0600

    second commit

--- WebAtelier/WebAtelier/settings.py
+++ WebAtelier/WebAtelier/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'WebAtelier.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_DIR,"templates"),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
